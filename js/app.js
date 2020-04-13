5cfec0ba95fcb9591cd8a42e9e9a2a78e4a9f28c
宜志富紹太 <shotagishitomi@gmail.com>
2020-04-14 01:57:07 +0900

    ドロワーメニューを作成！！

--- js/app.js
+++ js/app.js
@@ -11,8 +11,22 @@ $(function() {
     $('.ham').click(function() {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
+            $('.side-menu').removeClass('open');
+            $('#index').removeClass('open');
+            $('.overlay').removeClass('open');
         } else {
             $(this).addClass('active');
+            $('.side-menu').addClass('open');
+            $('#index').addClass('open');
+            $('.overlay').addClass('open');
+        }
+    });
+    $('.overlay').click(function() {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $('.ham').removeClass('active');
+            $('.side-menu').removeClass('open');
+            $('#index').removeClass('open');
         }
     });
 
